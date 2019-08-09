c3ed26ff12c68d08f374e53f9b658737651b14a1
Veronica Armour <vlarmour@gmail.com>
2019-08-09 15:58:21 -0400

    Replced PSU with SCI

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar-top">
          <div align=center>
-            <?php include('psuimg.php'); ?>
+            <?php include('sciimg.php'); ?>
             <h1 class="h3 text-center">
                <?php echo $ia['top']['title']; ?>
             </h1>
