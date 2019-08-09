6d83a632d621a3d86e1a85a3ef627cfe99e0eedd
Veronica Armour <vlarmour@gmail.com>
2019-08-09 15:59:11 -0400

    Delete psuimg.php

--- inc/psuimg.php
+++
@@ -1,2 +0,0 @@
-            <img src="img/RU_SHIELD_SIG_SCI.gif" alt="<?php echo $ia['top']['imgalt']; ?>"
-                 width="200" class="img-responsive center-block" vspace="10" longdesc="<?php echo $ia['top']['longdesc']; ?>">
